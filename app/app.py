1b03ee0c0f9fa8afa65b653e52a596ff76c25f99
Deyvid Kennedy Duarte Lucena <00001103527642sp@aluno.educacao.sp.gov.br>
2026-05-29 17:03:05 -0300

    atulição do codigo

--- app/app.py
+++ app/app.py
@@ -1,1 +1,16 @@
+import os
 
+def conectar_sistema():
+    db_user = os.getenv("DB_USER", "usuario_padrao")
+    db_password = os.getenv("DB_PASSWORD", "senha_padrao")
+
+    print("Conectando ao sistema...")
+    print(f"Usuário: {db_user}")
+    print("Senha: ***OCULTA***")  # nunca mostrar senha real
+
+    if db_password == "senha_padrao":
+        print("⚠️ Senha não configurada!")
+    else:
+        print("✔ Sistema conectado com segurança!")
+
+conectar_sistema()
